2c61ee9644acf5767bf4b6c8ab9f890318606854
Thiago Andreotti <dmpk02@gmail.com>
2023-07-26 22:17:28 -0300

    orangerie f v1.05

--- src/pages/Home/Posts.js
+++ src/pages/Home/Posts.js
@@ -76,7 +76,8 @@ const Posts = () => {
           <h2 className="no-photos">
             Ainda não temos fotos, a primeira postagem é sua, mãe{" "}
           </h2>
-          {user.userId && <Link to={`/users/${user.userId}`}>Clique Aqui</Link>}
+          {/* {user.userId && <Link to={`/users/${user.userId}`}>Clique Aqui</Link>} */}
+          <Link to={`/users/${user.userId}`}>Clique Aqui</Link>
         </>
       )}
     </div>
